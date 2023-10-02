b5dd41ecdbaf51550203958488300224076235cb
РомаН <119739400+romausnul@users.noreply.github.com>
2023-10-02 10:05:55 +0300

    Add files via upload

---
+++ WpfApp3/WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+
+namespace LibraryAuthentication
+{
+    public partial class MainWindow : Window
+    {
+        private string username = "admin";
+        private string password = "123456";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string enteredUsername = UsernameTextBox.Text;
+            string enteredPassword = PasswordBox.Password;
+
+            if (enteredUsername == username && enteredPassword == password)
+            {
+                MessageBox.Show("Авторизация успешна! Добро пожаловать, " + enteredUsername + "!");
+              
+            }
+            else
+            {
+                MessageBox.Show("Неверное имя пользователя или пароль. Попробуйте ещё раз.");
+            }
+        }
+    }
+}
